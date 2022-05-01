d340fc1b1727c925a887bdded8b3c1ba50a80fc8
dasecure <45675441+dasecure@users.noreply.github.com>
2022-04-30 17:59:03 -0700

    Simple method

--- main.go
+++ main.go
@@ -2,8 +2,25 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
+type Budget struct {
+	CampaignID string
+	Balance    float64
+	Expires    time.Time
+}
+
+func (b Budget) TimeLeft() time.Duration {
+	return b.Expires.Sub(time.Now().UTC())
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	b := Budget{
+		CampaignID: "Kittens",
+		Balance:    100,
+		Expires:    time.Now().UTC().Add(time.Hour * 24 * 7),
+	}
+
+	fmt.Println(b.TimeLeft())
 }
