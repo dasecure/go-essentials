61ad3aef1d4c25c3593bf887871d531b87e9635e
dasecure <45675441+dasecure@users.noreply.github.com>
2022-05-02 20:23:39 -0700

    experiments

--- main.go
+++ main.go
@@ -1,38 +1,15 @@
+// Open a text file and write into it
+
 package main
 
 import (
-	"fmt"
-	"math"
-	"time"
+	"log"
+	"os"
 )
 
 func main() {
-	ch1, ch2 := make(chan int), make(chan int)
-
-	go func() {
-		ch1 <- 42
-	}()
-
-	select {
-	case res := <-ch1:
-		fmt.Println("ch1:", res)
-	case res := <-ch2:
-		fmt.Println("ch2:", res)
+	err := os.WriteFile("./hellow", []byte("Hello, Gophers!"), 0666)
+	if err != nil {
+		log.Fatal(err)
 	}
-
-	fmt.Println("--------------------")
-
-	out := make(chan float64)
-	go func() {
-		time.Sleep(20 * time.Millisecond)
-		out <- math.Pi
-	}()
-
-	select {
-	case res := <-out:
-		fmt.Println("out:", res)
-	case <-time.After(20 * time.Millisecond):
-		fmt.Println("timeout")
-	}
-
 }
